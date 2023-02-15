47cfd0d4b8626ef0a270fc0a673271067238e8c4
Cristina-Andreea Alexe <alexe.cristina99@gmail.com>
2023-02-15 16:32:22 +0100

    added cuts

--- data_hist_producer.py
+++ data_hist_producer.py
@@ -1,4 +1,4 @@
-# Code to produce histograms from the data per eta, phi region
+# Stand alone code to produce histograms from data per eta, phi region
 
 from ROOT import TFile, TLorentzVector, RDataFrame, TH1D, TH2D, TCanvas, TGraph
 from ROOT import gROOT
@@ -7,6 +7,8 @@ from ROOT import Math
 import ROOT
 from array import array
 import numpy as np
+import os
+from os.path import exists
 
 # this is to supress ROOT garbage collector for histograms
 ROOT.TH1.AddDirectory(False)
@@ -22,71 +24,41 @@ def frame():
     ROOT.EnableImplicitMT(32) 
 
     names = ROOT.std.vector('string')()
-    #NanoV9Run2016FDataPostVFP_TrackFitV718_NanoProdv1
-    for n in range(2,101):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016FDataPostVFP_TrackFitV718_NanoProdv1/221230_011433/0000/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-        
-    for n in range(102,185):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016FDataPostVFP_TrackFitV718_NanoProdv1/221230_011433/0000/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
+    files=[]
 
-    #NanoV9Run2016GDataPostVFP_TrackFitV718_NanoProdv1    
-    for n in range(1,1000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016GDataPostVFP_TrackFitV718_NanoProdv1/221230_011512/0000/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
+    for root, dirnames, filenames in os.walk("/scratchnvme/wmass/NANOV9/postVFP/SingleMuon"):
+        for filename in filenames:
+            if '.root' in filename:
+                files.append(os.path.join(root, filename))
 
-    for n in range(1000,2000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016GDataPostVFP_TrackFitV718_NanoProdv1/221230_011512/0001/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-
-    for n in range(2000,3000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016GDataPostVFP_TrackFitV718_NanoProdv1/221230_011512/0002/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-
-    for n in range(3000,3449):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016GDataPostVFP_TrackFitV718_NanoProdv1/221230_011512/0003/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-
-    #NanoV9Run2016HDataPostVFP_TrackFitV718_NanoProdv1    
-    for n in range(1,1000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016HDataPostVFP_TrackFitV718_NanoProdv1/221230_011551/0000/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-
-    for n in range(1000,2000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016HDataPostVFP_TrackFitV718_NanoProdv1/221230_011551/0001/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-
-    for n in range(2000,3000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016HDataPostVFP_TrackFitV718_NanoProdv1/221230_011551/0002/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-
-    for n in range(3000,4000):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016HDataPostVFP_TrackFitV718_NanoProdv1/221230_011551/0003/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
-        
-    for n in range(4000,4596):
-        filename = f"/scratchnvme/wmass/NANOV9/postVFP/SingleMuon/NanoV9Run2016HDataPostVFP_TrackFitV718_NanoProdv1/221230_011551/0004/NanoV9DataPostVFP_{n}.root"
-        names.push_back(filename)
+    for name in files:
+        names.push_back(name)
         
     frame = RDataFrame("Events", names)
 
-    # Muon selection, to be refined later
+    # Muon selection
     frame = frame.Filter("HLT_IsoMu24 == 1")
     frame = frame.Filter("nMuon >= 2")
-    
-    # tight pID 
-    # pT cut
+    frame = frame.Filter("PV_npvsGood >= 1")
 
-    # Define invariant mass per mu pairs and filter through invariant mass
-    frame = frame.Define("pairs","std::vector<std::tuple<int,int,double>> pairs; for(int i=1;i<nMuon;i++){for(int j=0;j<i;j++){if(Muon_charge[i]*Muon_charge[j]==-1){double mll=pow(2*Muon_pt[i]*Muon_pt[j]*(cosh(Muon_eta[i]-Muon_eta[j])-cos(Muon_phi[i]-Muon_phi[j])),0.5); if(60<mll && mll<120){std::tuple<int,int,double> temp(i,j,mll); pairs.push_back(temp);}}}} return pairs;")
-    frame = frame.Filter("pairs.size()>=1")
+    #Muon_highPurity
+
+    frame = frame.Define("MuonisGood", "Muon_pt > 10 && abs(Muon_eta) < 2.4 && Muon_isGlobal && Muon_mediumId")
     
+    # Define invariant mass per mu pairs and filter through invariant mass
+    frame = frame.Define("pairs","std::vector<std::tuple<int,int,double>> pairs; for(int i=1;i<nMuon;i++){for(int j=0;j<i;j++){if(MuonisGood[i] && MuonisGood[j] && Muon_charge[i]*Muon_charge[j]==-1){double mll=pow(2*Muon_pt[i]*Muon_pt[j]*(cosh(Muon_eta[i]-Muon_eta[j])-cos(Muon_phi[i]-Muon_phi[j])),0.5); if(60<mll && mll<120){std::tuple<int,int,double> temp(i,j,mll); pairs.push_back(temp);}}}} return pairs;")
+    frame = frame.Filter("pairs.size()>=1")
+
+    # Define mll branch inclusive in all eta,phi bins
+    frame = frame.Define("mll","std::vector<double> temporary; for(int i=0; i<pairs.size(); i++){temporary.push_back(get<2>(pairs.at(i)));} return temporary;")
+     
     # Binning in eta {-2.4,-1.6,-0.8,0,0.8,1.6,2.4}
-    frame = frame.Define("etabin","std::array<double, 2> etabin; for(int j=0;j<2;j++){etabin[j]=-99999;} double etabins[7]={-2.4,-1.6,-0.8,0,0.8,1.6,2.4}; for(int k=0;k<2;k++){for(int i=0;i<6;i++){if(etabins[i]<Muon_eta[k] && Muon_eta[k]<etabins[i+1]){etabin[k]=i+1;}}} return etabin;")
+    frame = frame.Define("etabin","std::vector<int> etabin; for(int j=0;j<nMuon;j++){etabin.push_back(-99999);} double etabins[7]={-2.4,-1.6,-0.8,0,0.8,1.6,2.4}; for(int k=0;k<nMuon;k++){for(int i=0;i<6;i++){if(etabins[i]<Muon_eta[k] && Muon_eta[k]<etabins[i+1]){etabin[k]=i+1;}}} return etabin;")
            
     # Binning in phi {-3,-2,-1,0,1,2,3}    
-    frame = frame.Define("phibin","std::array<double, 2> phibin; for(int j=0;j<2;j++){phibin[j]=-99999;} double phibins[7]={-3,-2,-1,0,1,2,3}; for(int k=0;k<2;k++){for(int i=0;i<6;i++){if(phibins[i]<Muon_phi[k] && Muon_phi[k]<phibins[i+1]){phibin[k]=i+1;}}} return phibin;")
+    frame = frame.Define("phibin","std::vector<int> phibin; for(int j=0;j<nMuon;j++){phibin.push_back(-99999);} double phibins[7]={-3,-2,-1,0,1,2,3}; for(int k=0;k<nMuon;k++){for(int i=0;i<6;i++){if(phibins[i]<Muon_phi[k] && Muon_phi[k]<phibins[i+1]){phibin[k]=i+1;}}} return phibin;")
+
+    #frame.Snapshot("outputTree", "outputFile.root",{"MuonisGood","etabin","phibin","Muon_pt","mll"})
     
     # Book histograms per eta,phi region            
 
@@ -108,7 +80,7 @@ def frame():
                     histo[var_name].GetYaxis().SetTitle("Events")
 
     # Writing the histograms in a root file
-    outfile = ROOT.TFile.Open("data_histos.root","RECREATE")
+    outfile = ROOT.TFile.Open("data_histos_cuts.root","RECREATE")
     outfile.cd()
     for x in range(1,7): # range changes depending on binning
         for y in range(1,x+1):
@@ -121,12 +93,14 @@ def frame():
     h1 = frame.Histo1D(ROOT.RDF.TH1DModel("pt", "Pt", 100, 0, 100),"Muon_pt")
     h2 = frame.Histo1D(ROOT.RDF.TH1DModel("eta", "Eta", 60, -3, 3),"Muon_eta")
     h3 = frame.Histo1D(ROOT.RDF.TH1DModel("phi", "Phi", 80, -4, 4),"Muon_phi")
-     
+    h4 = frame.Histo1D(ROOT.RDF.TH1DModel("mll", "mll", 60, 60, 120),"mll")
+    
     outfilem = ROOT.TFile.Open("other_histos.root","RECREATE")
     outfilem.cd()
     h1.Write()
     h2.Write()
     h3.Write()
+    h4.Write()
     
     outfilem.Close()
 
