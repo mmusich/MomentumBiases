ccccd3d6d89c8d551cd616f467ace310770fb4ba
c-alexe <alexe.cristina99@gmail.com>
2023-11-21 15:39:52 +0100

    count events

--- sagitta_bin_debug.C
+++ sagitta_bin_debug.C
@@ -169,8 +169,7 @@ int frame(){
 
   //d5.Snapshot("Events", "snapshot_output.root", {"Muon_pt", "Muon_charge", "recoPt", "genPt", "ptDiff", "GenPart_pdgId", "GenPart_pt"});
 
-<<<<<<< HEAD
-  int nbinseta=24, nbinspt=20;
+  int nbinseta=1, nbinspt=1;
   float ptlow=25.0, pthigh=55.0;
   //larger bins while debugging
   //int nbinseta=6, nbinspt=2;
@@ -180,45 +179,25 @@ int frame(){
   vector<float> etabinranges, ptbinranges;
   for (int i=0; i<=nbinseta; i++){etabinranges.push_back(-2.4 + i * 4.8/nbinseta);}
 
-  nbinspt = 10;
+  nbinspt = 1;
   for (int i=0; i<=nbinspt; i++){ptbinranges.push_back(ptlow + i * (pthigh-ptlow)/nbinspt);}
-=======
-  //int nbinseta=24, nbinspt=10;
-  float ptlow=25.0, pthigh=55.0;
-  //larger bins while debugging
-  int nbinseta=6, nbinspt=2;
 
-  vector<float> etabinranges, ptbinranges;
-  for (int i=0; i<=nbinseta; i++){etabinranges.push_back(-2.4 + i * 4.8/nbinseta);}
-  for (int i=0; i<=nbinspt; i++){ptbinranges.push_back(ptlow + i * (pthigh-ptlow)/nbinspt);}
-    
-  auto multi_hist = d5.HistoND<float, float, float, float, RVecF, float>({"multi_data_frame", "multi_data_frame", 5, {nbinseta, nbinspt, nbinseta, nbinspt, 24}, {-2.4,ptlow,-2.4,ptlow,-6}, {2.4,pthigh,2.4,pthigh,6}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","ptDiff","genWeight"});
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
-
-  TH1F *mean = new TH1F("mean", "gen-reco mean", 3, 0, 3);
-  mean->SetCanExtend(TH1::kAllAxes);
-
-  TH1F *sigma = new TH1F("sigma", "gen-reco sigma", 3, 0, 3);
-  sigma->SetCanExtend(TH1::kAllAxes);
-
-  std::cout << "There are " << multi_hist->Projection(4)->GetEntries() << " entries in the inclusive projection \n";
+  double initial_entries = 0.0;
+  auto multi_hist_proj = multi_hist->Projection(4);
+  initial_entries = multi_hist_proj->GetEntries();
+  std::cout << "There are " << initial_entries << " entries in the inclusive projection \n";
  
   TFile f("reco_gen_histos.root","recreate");
   
-  auto multi_hist_proj = multi_hist->Projection(4);
   multi_hist_proj->SetName("inclusive_proj_pt_diff");
   multi_hist_proj->SetTitle("reco - gen pT inclusive");
-  //multi_hist_proj->Write();
-  
-  TF1 *gaussianFunc = new TF1("gaussianFunc", "gaus", -6, 6);
-  multi_hist_proj->Fit(gaussianFunc, "Q");
   multi_hist_proj->Write();
-
+  /*
   multi_hist_proj = multi_hist->Projection(0);
   multi_hist_proj->SetName("inclusive_proj_pos_eta");
   multi_hist_proj->SetTitle("pos eta inclusive");
   multi_hist_proj->Write();
-
+  
   multi_hist->GetAxis(0)->SetRange(3, 3);
   multi_hist_proj = multi_hist->Projection(0);
   multi_hist_proj->SetName("0_reduced_bin");
@@ -273,14 +252,12 @@ int frame(){
   multi_hist_proj->SetName("inclusive_proj_neg_pt");
   multi_hist_proj->SetTitle("neg pt inclusive");
   multi_hist_proj->Write();
- 
-  std::map<string, vector<float>> GenRecoFit;
-  vector<float> fitresult;
+  */
 
   //optimisation starts
   
   string name, title, title_seed = "reco - gen pT "; 
-  
+  /*
   multi_hist->GetAxis(0)->SetRange(3, 3);
   multi_hist->GetAxis(1)->SetRange(1, 1);
   multi_hist->GetAxis(2)->SetRange(4, 4);
@@ -288,7 +265,7 @@ int frame(){
   multi_hist_proj = multi_hist->Projection(4);
   multi_hist_proj->SetName("3_1_4_1_b");
   multi_hist_proj->Write();
-
+  
   multi_hist->GetAxis(0)->SetRangeUser(etabinranges[2]+0.00001, etabinranges[3]-0.00001);
   std::cout<<"\n etap "<<etabinranges[2]<<" "<<etabinranges[3]<<" ";
   multi_hist->GetAxis(1)->SetRangeUser(ptbinranges[0]+0.00001, ptbinranges[1]-0.00001);
@@ -300,104 +277,52 @@ int frame(){
   multi_hist_proj = multi_hist->Projection(4);
   multi_hist_proj->SetName("3_1_4_1_u");
   multi_hist_proj->Write();
+  */
 
-  TH2F* empty_histos = new TH2F("empty_histos", " fraction empty histos mu+", nbinseta, -2.4, 2.4, nbinspt, ptlow, pthigh);
-  TH2F* total_histos = new TH2F("total_histos", " total histos", nbinseta, -2.4, 2.4, nbinspt, ptlow, pthigh);
-  int entries = 0, all_histos_count = 0, empty_histos_count = 0;
-<<<<<<< HEAD
-  float events_difference = 0.0;
-=======
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
-  
-  for (int pos_eta_bin=1; pos_eta_bin<=nbinseta; pos_eta_bin++){
-    for (int pos_pt_bin=1; pos_pt_bin<=nbinspt; pos_pt_bin++){
-      for (int neg_eta_bin=1; neg_eta_bin<=nbinseta; neg_eta_bin++){
-	for (int neg_pt_bin=1; neg_pt_bin<=nbinspt; neg_pt_bin++){
-<<<<<<< HEAD
+  double events_count = 0.0, entries = 0.0;
+  //with under/overflow, labels don t make sense, can t use setrangeuser
+  for (int pos_eta_bin=0; pos_eta_bin<=nbinseta+1; pos_eta_bin++){
+    for (int pos_pt_bin=0; pos_pt_bin<=nbinspt+1; pos_pt_bin++){
+      for (int neg_eta_bin=0; neg_eta_bin<=nbinseta+1; neg_eta_bin++){
+	for (int neg_pt_bin=0; neg_pt_bin<=nbinspt+1; neg_pt_bin++){
+
 	  delete gROOT->FindObject("multi_data_frame_proj_4");
-=======
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
+
           name = stringify_name(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin);
 	  std::cout<<"\n"<< name;
+	  
           multi_hist->GetAxis(0)->SetRange(pos_eta_bin, pos_eta_bin);
 	  multi_hist->GetAxis(1)->SetRange(pos_pt_bin, pos_pt_bin);
           multi_hist->GetAxis(2)->SetRange(neg_eta_bin, neg_eta_bin);
           multi_hist->GetAxis(3)->SetRange(neg_pt_bin, neg_pt_bin);
           multi_hist_proj = multi_hist->Projection(4); 
 
-          entries = multi_hist_proj->GetEntries();
-	  std::cout<<"in histo "<<name<<" there are with SetRange "<< entries << " entries and with SetRangeUser there are ";          
-
+          //entries = multi_hist_proj->GetEntries();
+	  //std::cout<<"in histo "<<name<<" there are with SetRange "<< entries << " entries and with SetRangeUser there are ";          
+	  /*
+	  delete gROOT->FindObject("multi_data_frame_proj_4");
           multi_hist->GetAxis(0)->SetRangeUser(etabinranges[pos_eta_bin - 1]+0.00001, etabinranges[pos_eta_bin]-0.0001);
 	  multi_hist->GetAxis(1)->SetRangeUser(ptbinranges[pos_pt_bin - 1]+0.00001, ptbinranges[pos_pt_bin]-0.0001);
           multi_hist->GetAxis(2)->SetRangeUser(etabinranges[neg_eta_bin - 1]+0.00001, etabinranges[neg_eta_bin]-0.0001);
           multi_hist->GetAxis(3)->SetRangeUser(ptbinranges[neg_pt_bin - 1]+0.00001, ptbinranges[neg_pt_bin]-0.0001);
           multi_hist_proj = multi_hist->Projection(4);
-
-<<<<<<< HEAD
-	  events_difference += entries - multi_hist_proj->GetEntries();
-=======
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
-	  std::cout<<multi_hist_proj->GetEntries()<< " and the difference: "<< entries - multi_hist_proj->GetEntries()<<"\n";
-
-          //entries = multi_hist_proj->GetEntries();
-          //total_histos->Fill(etabinranges[pos_eta_bin-1]+0.001, ptbinranges[pos_pt_bin-1]+0.001);
-          all_histos_count++;
-          if (entries < 50){ 
-            //empty_histos->Fill(etabinranges[pos_eta_bin-1]+0.001, ptbinranges[pos_pt_bin-1]+0.001);
-            empty_histos_count++;
-            continue;
-          } else {
+	  */
+	  entries = multi_hist_proj->GetEntries();
+	  std::cout<<"entries: "<<entries;
+	  events_count =  events_count + entries;
 	  
-<<<<<<< HEAD
-            /*
-=======
-            
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
-	    //name = stringify_name(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin);
-            title = stringify_title(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin, etabinranges, ptbinranges);
-            multi_hist_proj->SetName(name.c_str());
-            multi_hist_proj->SetTitle(title.c_str());
-
-	    fitresult = fitHisto(multi_hist_proj);
-            GenRecoFit[name] = fitresult;
-            mean->Fill(name.c_str(), fitresult[0]);
-	    sigma->Fill(name.c_str(), fitresult[1]);
-            multi_hist_proj->Write(name.c_str());
-<<<<<<< HEAD
-            */
-=======
-            
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
-	  }
+          title = stringify_title(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin, etabinranges, ptbinranges);
+          multi_hist_proj->SetName(name.c_str());
+          multi_hist_proj->SetTitle(title.c_str());
+          multi_hist_proj->Write(name.c_str());
+	  
 	}
       } 
     }
   } 
-<<<<<<< HEAD
 
-  std::cout<<"ev diff"<<events_difference; 
-
-=======
+  std::cout<<"Initial no. of events: "<<initial_entries<<"sum of events in individual histos: "<<events_count<<" ev diff: "<<initial_entries-events_count; 
  
->>>>>>> d846c300b6e6843250729878880ec9b3e9685bd2
-  /*
-  empty_histos->Divide(total_histos);
-  empty_histos->Write("empty_histos");    
-  auto c = new TCanvas("c","c");
-  c->SetRightMargin(0.20);
-  empty_histos->Draw("COLZ");
-  c->SaveAs("empty_histos.pdf");
-  
-  mean->SetStats(0);
-  mean->LabelsDeflate();
-  mean->Write("mean");
-  
-  sigma->SetStats(0);
-  sigma->LabelsDeflate();
-  sigma->Write("sigma");
-  */
-
   f.Close();
 
   return 0; 
