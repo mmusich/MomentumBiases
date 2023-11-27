84c73ca159af69172a8e8782815186bc18d8efde
c-alexe <alexe.cristina99@gmail.com>
2023-11-27 19:25:37 +0100

    save mll diff

--- multiDhist_producer.C
+++ multiDhist_producer.C
@@ -7,36 +7,12 @@
 using namespace ROOT;
 using namespace ROOT::VecOps;
 
-// MuonGenMatchedIndex
-// works as it is because there is only one muon pair selected  per event
-// TODO try a match by deltaR instead
-RVecF MuonGenMatchedIndex(RVecI GenPart_status, RVecI GenPart_pdgId, RVecI GenPart_genPartIdxMother, RVecF GenPart_pt){
-  int posindex = 999;
-  int negindex = 999;
-  RVecF genpt;
-  for(int i=0;i<GenPart_status.size();i++){
-    if (GenPart_status[i] == 1 && GenPart_pdgId[GenPart_genPartIdxMother[i]] == 23){
-      if (GenPart_pdgId[i] == -13){ // mu(-) has PDGID 13
-        posindex = i;
-      } else if(GenPart_pdgId[i] == 13) {
-        negindex = i;
-      }
-    }
-  }
-  genpt.push_back(GenPart_pt[posindex]);
-  genpt.push_back(GenPart_pt[negindex]);
-    
-  return genpt;
-}
-
 //dxy_significance
-
 RVecF dxy_significance(RVecF Muon_dxy, RVecF Muon_dxyErr){
   return abs(Muon_dxy)/Muon_dxyErr;
 }
 
 // MuonisGood
-
 RVecB MuonisGood(RVecF Muon_pt, RVecF Muon_eta, RVecB Muon_isGlobal, RVecB Muon_mediumId, RVecF Muon_pfRelIso04_all, RVecF dxy_significance){
   RVecB muonisgood;
   for(int i=0;i<Muon_pt.size();i++){
@@ -48,11 +24,10 @@ RVecB MuonisGood(RVecF Muon_pt, RVecF Muon_eta, RVecB Muon_isGlobal, RVecB Muon_
 }
 
 //Pairs
+std::tuple<int,int,float, float> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Muon_eta, RVecF Muon_phi, RVecB MuonisGood, RVecF Muon_dxy, RVecF Muon_dz, float rest_mass, RVecI GenPart_status, RVecI GenPart_pdgId, RVecI GenPart_genPartIdxMother, RVecF GenPart_pt, RVecF GenPart_eta, RVecF GenPart_phi){
 
-RVec<std::tuple<int,int,float>> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Muon_eta, RVecF Muon_phi, RVecB MuonisGood, RVecF Muon_dxy, RVecF Muon_dz, float rest_mass){
-
-  RVec<std::tuple<int,int,float>> pairs; // <pos_muon_index, neg_muon_index, pair_inv_mass>
-  std::tuple<int,int,float> temp;
+  RVec<std::tuple<int,int,float,float>> pairs; // <pos_muon_index, neg_muon_index, pair_inv_mass>
+  std::tuple<int,int,float,float> temp, pair_to_return;
 
   for(int i=1;i<Muon_pt.size();i++){
     for(int j=0;j<i;j++){
@@ -61,16 +36,37 @@ RVec<std::tuple<int,int,float>> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Mu
 	float gamma_angle = 0;
 	gamma_angle = acos(sin(2*atan(exp((-1)*Muon_eta[i])))*sin(2*atan(exp((-1)*Muon_eta[j])))*cos(Muon_phi[i])*cos(Muon_phi[j]) + sin(2*atan(exp((-1)*Muon_eta[i])))*sin(2*atan(exp((-1)*Muon_eta[j])))*sin(Muon_phi[i])*sin(Muon_phi[j]) + cos(2*atan(exp((-1)*Muon_eta[i])))*cos(2*atan(exp((-1)*Muon_eta[j]))));
 	if(gamma_angle > 3.141592653/4){
-	  TLorentzVector firstTrack, secondTrack, mother;
+	  TLorentzVector firstTrack, secondTrack, mother, firstGenTrack, secondGenTrack, motherGen;
 	  firstTrack.SetPtEtaPhiM(Muon_pt[i], Muon_eta[i], Muon_phi[i], rest_mass);  
 	  secondTrack.SetPtEtaPhiM(Muon_pt[j], Muon_eta[j], Muon_phi[j], rest_mass);
 	  mother = firstTrack + secondTrack;
 	  float mll = mother.M();
-	  if(75<mll && mll<105){
+	  if(75<mll && mll<105){ //Cut in mll
+	    //Gen match
+	    bool firstGenMatched = false, secondGenMatched = false;
+	    for (int k=0;k<GenPart_eta.size();k++){
+	      if (GenPart_status[k]==1 && abs(GenPart_pdgId[k])==13 && GenPart_pdgId[GenPart_genPartIdxMother[k]]==23){ // mu(-) has PDGID 13
+		if(abs(pow(GenPart_eta[k],2) + pow(GenPart_phi[k],2) - pow(Muon_eta[i],2) - pow(Muon_phi[i],2))<0.15){ 
+		  //CHECK IF deltaR value is large enough, maybe more than a gen muon satisfies the condition
+		  firstGenTrack.SetPtEtaPhiM(GenPart_pt[k], GenPart_eta[k], GenPart_phi[k], rest_mass);
+		  firstGenMatched = true;
+		} else if(abs(pow(GenPart_eta[k],2) + pow(GenPart_phi[k],2) - pow(Muon_eta[j],2) - pow(Muon_phi[j],2))<0.15){
+		  secondGenTrack.SetPtEtaPhiM(GenPart_pt[k], GenPart_eta[k], GenPart_phi[k], rest_mass);
+		  secondGenMatched = true;
+		}
+	      }
+	    }	  
+	    if(firstGenMatched == false || secondGenMatched == false){
+	      continue; 
+	    }
+	    motherGen = firstGenTrack + secondGenTrack;
+	    float mllGen = motherGen.M();
+	    float mll_diff = mll - mllGen;
+	    
 	    if(Muon_charge[i]==1){
-	      temp=make_tuple(i,j,mll);
+	      temp=make_tuple(i,j,mll,mll_diff);
 	    } else {
-	      temp=make_tuple(j,i,mll);
+	      temp=make_tuple(j,i,mll,mll_diff);
 	    }
 	    pairs.push_back(temp);
 	  }
@@ -78,7 +74,10 @@ RVec<std::tuple<int,int,float>> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Mu
       }
     }
   }
-  if(pairs.size()>1){
+
+  if(pairs.size()==1){
+    pair_to_return=pairs.at(0);
+  } else if(pairs.size()>1){
     float diff=100.0;
     int best=0;
     for(int i=0;i<pairs.size();i++){
@@ -87,11 +86,11 @@ RVec<std::tuple<int,int,float>> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Mu
 	best=i;
       }
     }
-    RVec<std::tuple<int,int,float>> pairss;
-    pairss.push_back(pairs.at(best));
-    return pairss;
-  }
-  return pairs;
+    pair_to_return=pairs.at(best);
+  } else {
+    pair_to_return=make_tuple(0,0,0.0,0.0);
+ }
+  return pair_to_return;
 }
 
 int frame(){
@@ -120,38 +119,36 @@ int frame(){
 
   auto d2 = d1.Define("dxy_significance","dxy_significance(Muon_dxy, Muon_dxyErr)")
     .Define("MuonisGood", "MuonisGood(Muon_pt, Muon_eta, Muon_isGlobal, Muon_mediumId, Muon_pfRelIso04_all, dxy_significance)")
-    .Define("pairs", "pairs(Muon_pt, Muon_charge, Muon_eta, Muon_phi, MuonisGood, Muon_dxy, Muon_dz, 0.105658)"); // muMass = 0.105658 GeV
-  
-  auto d3 = d2.Filter("pairs.size()==1");
+    .Define("pairs", "pairs(Muon_pt, Muon_charge, Muon_eta, Muon_phi, MuonisGood, Muon_dxy, Muon_dz, 0.105658, GenPart_status, GenPart_pdgId, GenPart_genPartIdxMother, GenPart_pt, GenPart_eta, GenPart_phi)") 
+    // muMass = 0.105658 GeV
+    .Define("mll_diff","return get<3>(pairs);")
+    .Define("mll","return get<2>(pairs);");  
+  auto d3 = d2.Filter("mll>70"); // this means only events with one mu pair are kept 
 
   // This below works because actually we kept only one pair per event
   // Accessing properties through the idices of pairs ensures the muons passed MuonisGood
   
-  auto d4 = d3.Define("posTrackPt","float posTrackPt; posTrackPt=Muon_pt[get<0>(pairs.at(0))]; return posTrackPt;")
-    .Define("negTrackPt","float negTrackPt; negTrackPt=Muon_pt[get<1>(pairs.at(0))]; return negTrackPt;")
-    .Define("recoPt","RVecF recoPt; recoPt.push_back(posTrackPt); recoPt.push_back(negTrackPt); return recoPt;")
-    .Define("genPt", "MuonGenMatchedIndex(GenPart_status, GenPart_pdgId, GenPart_genPartIdxMother, GenPart_pt)")
-    .Define("posTrackEta","float posTrackEta; posTrackEta=Muon_eta[get<0>(pairs.at(0))]; return posTrackEta;")
-    .Define("negTrackEta","float negTrackEta; negTrackEta=Muon_eta[get<1>(pairs.at(0))]; return negTrackEta;")
-    .Define("mll","RVecF temporary; for(int i=0; i<pairs.size(); i++){temporary.push_back(get<2>(pairs.at(i)));} return temporary;");
+  auto d4 = d3.Define("posTrackPt","float posTrackPt; posTrackPt=Muon_pt[get<0>(pairs)]; return posTrackPt;")
+    .Define("negTrackPt","float negTrackPt; negTrackPt=Muon_pt[get<1>(pairs)]; return negTrackPt;")
+    .Define("posTrackEta","float posTrackEta; posTrackEta=Muon_eta[get<0>(pairs)]; return posTrackEta;")
+    .Define("negTrackEta","float negTrackEta; negTrackEta=Muon_eta[get<1>(pairs)]; return negTrackEta;");
 
-  auto d5 = d4.Define("ptDiff","RVecF ptDiff; ptDiff.push_back(recoPt[0] - genPt[0]); ptDiff.push_back(recoPt[1] - genPt[1]); return ptDiff;");
-
-  //TFile *f1 = new TFile("snapshot_output.root","RECREATE");
-  //d5.Snapshot("Events", "snapshot_output.root", {"Muon_pt", "Muon_charge", "recoPt", "genPt", "ptDiff", "GenPart_pdgId", "GenPart_pt"});
-
+  TFile *f1 = new TFile("snapshot_output.root","RECREATE");
+  d4.Snapshot("Events", "snapshot_output.root", {"Muon_pt", "GenPart_pdgId", "GenPart_pt", "GenPart_status", "mll", "mll_diff"});
+  
+  /*
   //number of eta pt bins should match or be larger than the one chosen later
-  int nbinseta=24, nbinspt=30;
+  int nbinseta=24, nbinspt=50;
   float ptlow=25.0, pthigh=55.0;
   //larger bins while debugging
   //int nbinseta=6, nbinspt=2;
     
-  auto multi_hist = d5.HistoND<float, float, float, float, RVecF, float>({"multi_data_frame", "multi_data_frame", 5, {nbinseta, nbinspt, nbinseta, nbinspt, 24}, {-2.4,ptlow,-2.4,ptlow,-6}, {2.4,pthigh,2.4,pthigh,6}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","ptDiff","genWeight"});
+  auto multi_hist = d4.HistoND<float, float, float, float, RVecF, float>({"multi_data_frame", "multi_data_frame", 5, {nbinseta, nbinspt, nbinseta, nbinspt, 24}, {-2.4,ptlow,-2.4,ptlow,-6}, {2.4,pthigh,2.4,pthigh,6}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","ptDiff","genWeight"});
 
   TFile f2("multiD_histo.root","recreate");
   multi_hist->Write(); 
   f2.Close();
-
+  */
   return 0;
 
 }
