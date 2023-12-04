f9c7e8fb0341562cb0cdb8a00a07dad38f449b25
c-alexe <alexe.cristina99@gmail.com>
2023-12-04 19:09:11 +0100

    plot 3 mll

--- multiDhist_producer.C
+++ multiDhist_producer.C
@@ -24,10 +24,10 @@ RVecB MuonisGood(RVecF Muon_pt, RVecF Muon_eta, RVecB Muon_isGlobal, RVecB Muon_
 }
 
 //Pairs
-std::tuple<int,int,float, float> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Muon_eta, RVecF Muon_phi, RVecB MuonisGood, RVecF Muon_dxy, RVecF Muon_dz, float rest_mass, RVecI GenPart_status, RVecI GenPart_pdgId, RVecI GenPart_genPartIdxMother, RVecF GenPart_pt, RVecF GenPart_eta, RVecF GenPart_phi){
+std::tuple<int,int,float,float,float> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF Muon_eta, RVecF Muon_phi, RVecB MuonisGood, RVecF Muon_dxy, RVecF Muon_dz, float rest_mass, RVecI GenPart_status, RVecI GenPart_pdgId, RVecI GenPart_genPartIdxMother, RVecF GenPart_pt, RVecF GenPart_eta, RVecF GenPart_phi){
 
-  RVec<std::tuple<int,int,float,float>> pairs; // <pos_muon_index, neg_muon_index, pair_inv_mass>
-  std::tuple<int,int,float,float> temp, pair_to_return;
+  RVec<std::tuple<int,int,float,float,float>> pairs; // <pos_muon_index, neg_muon_index, pair_inv_mass>
+  std::tuple<int,int,float,float,float> temp, pair_to_return;
 
   for(int i=1;i<Muon_pt.size();i++){
     for(int j=0;j<i;j++){
@@ -36,8 +36,8 @@ std::tuple<int,int,float, float> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF M
 	firstTrack.SetPtEtaPhiM(Muon_pt[i], Muon_eta[i], Muon_phi[i], rest_mass);  
 	secondTrack.SetPtEtaPhiM(Muon_pt[j], Muon_eta[j], Muon_phi[j], rest_mass);
 	mother = firstTrack + secondTrack;
-	float mll = mother.M();
-	if(75.0<mll && mll<105.0){ //Cut in mll
+	float mll_reco = mother.M();
+	if(75.0<mll_reco && mll_reco<105.0){ //Cut in mll
 	  //Gen match
 	  bool firstGenMatched = false, secondGenMatched = false;
 	  for (int k=0;k<GenPart_eta.size();k++){
@@ -57,13 +57,13 @@ std::tuple<int,int,float, float> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF M
 	    continue; 
 	  }
 	  motherGen = firstGenTrack + secondGenTrack;
-	  float mllGen = motherGen.M();
-	  float mll_diff = mll - mllGen;
+	  float mll_gen = motherGen.M();
+	  float mll_diff = mll_reco - mll_gen;
 	  
 	  if(Muon_charge[i]==1){
-	    temp=make_tuple(i,j,mll,mll_diff);
+	    temp=make_tuple(i,j,mll_reco,mll_gen,mll_diff);
 	  } else {
-	    temp=make_tuple(j,i,mll,mll_diff);
+	    temp=make_tuple(j,i,mll_reco,mll_gen,mll_diff);
 	  }
 	  pairs.push_back(temp);
 	}	
@@ -84,7 +84,7 @@ std::tuple<int,int,float, float> pairs(RVecF Muon_pt, RVecF Muon_charge, RVecF M
     }
     pair_to_return=pairs.at(best);
   } else {
-    pair_to_return=make_tuple(0,0,0.0,0.0);
+    pair_to_return=make_tuple(0,0,0.0,0.0,0.0);
  }
   return pair_to_return;
 }
@@ -117,14 +117,15 @@ int frame(){
     .Define("MuonisGood", "MuonisGood(Muon_pt, Muon_eta, Muon_isGlobal, Muon_mediumId, Muon_pfRelIso04_all, Muon_genPartFlav, dxy_significance)")
     .Define("pairs", "pairs(Muon_pt, Muon_charge, Muon_eta, Muon_phi, MuonisGood, Muon_dxy, Muon_dz, 0.105658, GenPart_status, GenPart_pdgId, GenPart_genPartIdxMother, GenPart_pt, GenPart_eta, GenPart_phi)") 
     // muMass = 0.105658 GeV
-    .Define("mll","return get<2>(pairs);"); 
+    .Define("mll_reco","return get<2>(pairs);"); 
  
-  auto d3 = d2.Filter("mll>70"); // this means only events with one mu pair are kept 
+  auto d3 = d2.Filter("mll_reco>70"); // this means only events with one mu pair are kept 
 
   // This below works because actually we kept only one pair per event
   // Accessing properties through the idices of pairs ensures the muons passed MuonisGood
   
-  auto d4 = d3.Define("mll_diff","return get<3>(pairs);")
+  auto d4 = d3.Define("mll_gen","return get<3>(pairs);")
+    .Define("mll_diff","return get<4>(pairs);")
     .Define("weight", "std::copysign(1.0, genWeight)")
     .Define("posTrackPt","float posTrackPt; posTrackPt=Muon_pt[get<0>(pairs)]; return posTrackPt;")
     .Define("negTrackPt","float negTrackPt; negTrackPt=Muon_pt[get<1>(pairs)]; return negTrackPt;")
@@ -133,34 +134,76 @@ int frame(){
 
   //Save tree for debugging
   //TFile *f1 = new TFile("snapshot_output.root","RECREATE");
-  //d4.Snapshot("Events", "snapshot_output.root", {"GenPart_status", "GenPart_pdgId", "GenPart_pt", "GenPart_eta", "GenPart_phi", "Muon_pt", "Muon_eta", "Muon_phi", "mll", "mll_diff"});
+  //d4.Snapshot("Events", "snapshot_output.root", {"GenPart_status", "GenPart_pdgId", "GenPart_pt", "GenPart_eta", "GenPart_phi", "Muon_pt", "Muon_eta", "Muon_phi", "mll_reco", "mll_gen", "mll_diff"});
 
   //Tree to pass to fitting script
-  TFile *f2 = new TFile("tree_output.root","RECREATE");
-  d4.Snapshot("Events", "tree_output.root",{"posTrackPt", "posTrackEta", "negTrackPt", "negTrackEta", "mll_diff", "mll", "weight"});
+  //TFile *f2 = new TFile("tree_output.root","RECREATE");
+  //d4.Snapshot("Events", "tree_output.root",{"posTrackPt", "posTrackEta", "negTrackPt", "negTrackEta", "mll_reco", "mll_gen", "mll_diff", "weight"});
   
   /*
   //Control histograms
-  auto mll_hist = d4.Histo1D({"mll", "mll inclusive all bins", 45, 75.0, 105.0},"mll");
+  auto mll_reco_hist = d4.Histo1D({"mll_reco", "mll inclusive all bins", 45, 75.0, 105.0},"mll_reco");
   auto mll_diff_hist = d4.Histo1D({"mll_diff", "mll diff inclusive all bins", 24, -6.0, 6.0},"mll_diff");
   TFile f3("control_histo.root","recreate");
-  mll_hist->Write();
+  mll_reco_hist->Write();
   mll_diff_hist->Write();
   f3.Close();
-
-  
-  //number of eta pt bins should match or be larger than the one chosen later
-  int nbinseta=24, nbinspt=50;
-  float ptlow=25.0, pthigh=55.0;
-  //larger bins while debugging
-  //int nbinseta=6, nbinspt=2;
-    
-  auto multi_hist = d4.HistoND<float, float, float, float, RVecF, float>({"multi_data_frame", "multi_data_frame", 5, {nbinseta, nbinspt, nbinseta, nbinspt, 24}, {-2.4,ptlow,-2.4,ptlow,-6}, {2.4,pthigh,2.4,pthigh,6}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","ptDiff","weight"});
-
-  TFile f4("multiD_histo.root","recreate");
-  multi_hist->Write(); 
-  f4.Close();
   */
+
+  double ptlow=25.0, pthigh=55.0;
+  int nbinsmll_diff=18, nbinsmll=18, nbinseta=24, nbinspt=5;
+  vector<double> etabinranges, ptbinranges, mll_diffbinranges, mllbinranges;
+
+  std::cout<<"\n etabinranges = [";
+  for (int i=0; i<=nbinseta; i++){etabinranges.push_back(-2.4 + i * 4.8/nbinseta); std::cout<<etabinranges[i]<<", ";}
+  std::cout<<"] \n";
+
+  double myetaboundaries[etabinranges.size()];
+  for (int i=0; i<etabinranges.size(); i++){
+    myetaboundaries[i] = etabinranges[i];
+  }
+
+  for (int i=0; i<=nbinsmll_diff; i++) mll_diffbinranges.push_back(-6.0 + i * 12.0/nbinsmll_diff);
+  for (int i=0; i<=nbinsmll; i++) mllbinranges.push_back(75.0 + i * (105.0-75.0)/nbinsmll);
+
+  std::unique_ptr<TFile> f4( TFile::Open("control_bin_histo.root", "RECREATE") );  
+
+  //pT bin optimisation starts
+  auto pt_pos_uni = d4.Histo1D({"pt_pos_uni", "pt mu+", nbinspt*3, ptlow, pthigh},"posTrackPt");
+  f4->WriteObject(pt_pos_uni.GetPtr(), "pt_pos_uni");
+  // Get quartiles
+  double xq[nbinspt+1], myptboundaries[nbinspt+1];
+  for (int i=0;i<=nbinspt;i++) xq[i] = float(i)/nbinspt;
+  pt_pos_uni->GetQuantiles(nbinspt+1,myptboundaries, xq);
+
+  ptbinranges.clear();
+  std::cout<<"ptbinranges = [";
+  for (int i=0; i<=nbinspt; i++){
+    ptbinranges.push_back(myptboundaries[i]);
+    std::cout<<ptbinranges[i]<<", ";
+  }
+  std::cout<<"] \n";
+
+  //TH1 in pT+ with variable bin size -> should be uniform
+  auto pt_pos = d4.Histo1D({"pt_pos", "pt mu+", nbinspt, myptboundaries},"posTrackPt");
+  f4->WriteObject(pt_pos.GetPtr(), "pt_pos");
+
+  auto pt_eta_pos = d4.Histo2D({"pt_eta_pos", "pt eta mu+", nbinseta, myetaboundaries, nbinspt, myptboundaries},"posTrackEta", "posTrackPt");
+  f4->WriteObject(pt_eta_pos.GetPtr(), "pt_eta_pos");
+
+  std::unique_ptr<TFile> f5( TFile::Open("multiD_histo.root", "RECREATE") );
+
+  auto mDh = d4.HistoND<float, float, float, float, float, double>({"multi_data_histo_reco", "multi_data_histo_reco", 5, {nbinseta, nbinspt, nbinseta, nbinspt, nbinsmll}, {etabinranges, ptbinranges, etabinranges, ptbinranges, mllbinranges}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","mll_reco","weight"});
+  f5->WriteObject(mDh.GetPtr(), "multi_data_histo_reco");
+  delete gROOT->FindObject("multi_data_histo_reco");
+
+  mDh = d4.HistoND<float, float, float, float, float, double>({"multi_data_histo_gen", "multi_data_histo_gen", 5, {nbinseta, nbinspt, nbinseta, nbinspt, nbinsmll}, {etabinranges, ptbinranges, etabinranges,ptbinranges, mllbinranges}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","mll_gen","weight"});
+  f5->WriteObject(mDh.GetPtr(), "multi_data_histo_gen");
+  delete gROOT->FindObject("multi_data_histo_gen");
+
+  mDh = d4.HistoND<float, float, float, float, float, double>({"multi_data_histo_diff", "multi_data_histo_diff", 5, {nbinseta, nbinspt, nbinseta, nbinspt, nbinsmll_diff}, {etabinranges, ptbinranges, etabinranges, ptbinranges, mll_diffbinranges}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","mll_diff","weight"});
+  f5->WriteObject(mDh.GetPtr(), "multi_data_histo_diff"); 
+  
   return 0;
 
 }
--- sagitta_bin_optimise.C
+++ sagitta_bin_optimise.C
@@ -12,12 +12,12 @@ string stringify_name(int a, int b, int c, int d){
 }
 
 string stringify_title(int a, int b, int c, int d, vector<double> e, vector<double> p){
-  string title_seed = "reco - gen mll ", comma = ",";
+  string comma = ",";
   string txt1 = "eta+ in [", txt2="] pt+ in [", txt3="] eta- in [", txt4="] pt- in [", txt5 = "]";
-  return title_seed+txt1+to_string(e[a-1]).substr(0,4)+comma+to_string(e[a]).substr(0,4)+txt2+to_string(p[b-1]).substr(0,4)+comma+to_string(p[b]).substr(0,4)+txt3+to_string(e[c-1]).substr(0,4)+comma+to_string(e[c]).substr(0,4)+txt4+to_string(p[d-1]).substr(0,4)+comma+to_string(p[d]).substr(0,4)+txt5;
+  return txt1+to_string(e[a-1]).substr(0,4)+comma+to_string(e[a]).substr(0,4)+txt2+to_string(p[b-1]).substr(0,4)+comma+to_string(p[b]).substr(0,4)+txt3+to_string(e[c-1]).substr(0,4)+comma+to_string(e[c]).substr(0,4)+txt4+to_string(p[d-1]).substr(0,4)+comma+to_string(p[d]).substr(0,4)+txt5;
 }
 
-vector<float> fitHisto(TH1* histogram){
+vector<float> fitHisto(TH1* histogram, int color){
 
   vector<float> fitresult;
 
@@ -32,7 +32,8 @@ vector<float> fitHisto(TH1* histogram){
     // second fit: two sigma of first fit around mean of first fit
     gaussianFunc->SetRange(mean - 2 * sigma, mean + 2 * sigma);
     if (0 == histogram->Fit(gaussianFunc, "Q0R")) {
-      if (histogram->GetFunction(gaussianFunc->GetName())) {  // Take care that it is later on drawn:
+      if (histogram->GetFunction(gaussianFunc->GetName())) { // Take care that it is later on drawn:
+	histogram->GetFunction(gaussianFunc->GetName())->SetLineColor(color);
         histogram->GetFunction(gaussianFunc->GetName())->ResetBit(TF1::kNotDraw);
       }
     }
@@ -56,26 +57,23 @@ int frame(){
 
   ROOT::EnableImplicitMT(128);
 
-  RDataFrame df("Events", "tree_output.root");
+  std::unique_ptr<TFile> myFile( TFile::Open("multiD_histo.root") );
+  std::unique_ptr<THnD> mDh_reco(myFile->Get<THnD>("multi_data_histo_reco"));
+  std::unique_ptr<THnD> mDh_gen(myFile->Get<THnD>("multi_data_histo_gen"));
+  std::unique_ptr<THnD> mDh_diff(myFile->Get<THnD>("multi_data_histo_diff"));
 
+  //these must match how the 5D histo was produced
   double ptlow=25.0, pthigh=55.0;
-  int nbinsmll_diff=18, nbinseta=24, nbinspt=5;
-  vector<double> etabinranges, ptbinranges, mll_diffbinranges;
+  int nbinsmll_diff=18, nbinsmll=18, nbinseta=24, nbinspt=5;
+  vector<double> etabinranges, ptbinranges{25.0, 33.3584, 38.4562, 42.2942, 45.9469, 55.0};
 
   std::cout<<"\n etabinranges = [";
   for (int i=0; i<=nbinseta; i++){etabinranges.push_back(-2.4 + i * 4.8/nbinseta); std::cout<<etabinranges[i]<<", ";}
   std::cout<<"] \n";
 
-  double myetaboundaries[etabinranges.size()];
-  for (int i=0; i<etabinranges.size(); i++){
-    myetaboundaries[i] = etabinranges[i];
-  }
-
-  for (int i=0; i<=nbinsmll_diff; i++) mll_diffbinranges.push_back(-6.0 + i * 12.0/nbinsmll_diff);
-
   //Initialise variables
   double total_nevents=0.0, nevents=0.0, all_histos_count=0.0, remaining_nevents=0.0, empty_histos_count=0.0, hfrac=-1.0, efrac=-1.0;
-  string name, title;
+  string name;
 
   std::map<string, vector<float>> GenRecoFit;
   vector<float> fitresult;
@@ -89,33 +87,11 @@ int frame(){
   std::unique_ptr<TFile> f1( TFile::Open("control_bin_histo.root", "RECREATE") );
   std::unique_ptr<TFile> f2( TFile::Open("reco_gen_histos.root", "RECREATE") );
 
-  //pT bin optimisation starts  
-  auto pt_pos_uni = df.Histo1D({"pt_pos_uni", "pt mu+", nbinspt*3, ptlow, pthigh},"posTrackPt");
-  f1->WriteObject(pt_pos_uni.GetPtr(), "pt_pos_uni");
-  // Get quartiles
-  double xq[nbinspt+1], myptboundaries[nbinspt+1];
-  for (int i=0;i<=nbinspt;i++) xq[i] = float(i)/nbinspt;
-  pt_pos_uni->GetQuantiles(nbinspt+1,myptboundaries, xq);
-  
-  ptbinranges.clear();
-  std::cout<<"ptbinranges = [";
-  for (int i=0; i<=nbinspt; i++){
-    ptbinranges.push_back(myptboundaries[i]);
-    std::cout<<ptbinranges[i]<<", ";
-  }
-  std::cout<<"] \n";
-  
-  //TH1 in pT+ with variable bin size -> should be uniform
-  auto pt_pos = df.Histo1D({"pt_pos", "pt mu+", nbinspt, myptboundaries},"posTrackPt");
-  f1->WriteObject(pt_pos.GetPtr(), "pt_pos");  
-  
-  auto pt_eta_pos = df.Histo2D({"pt_eta_pos", "pt eta mu+", nbinseta, myetaboundaries, nbinspt, myptboundaries},"posTrackEta", "posTrackPt");
-  f1->WriteObject(pt_eta_pos.GetPtr(), "pt_eta_pos");
-  
-  auto mDh = df.HistoND<float, float, float, float, float, double>({"multi_data_frame", "multi_data_frame", 5, {nbinseta, nbinspt, nbinseta, nbinspt, nbinsmll_diff}, {etabinranges, ptbinranges, etabinranges, ptbinranges, mll_diffbinranges}}, {"posTrackEta","posTrackPt","negTrackEta","negTrackPt","mll_diff","weight"});
-  
-  auto multi_hist_proj = mDh->Projection(4);
-  total_nevents = multi_hist_proj->Integral(1,nbinsmll_diff);
+  auto multi_hist_proj_diff = mDh_diff->Projection(4);
+  auto multi_hist_proj_reco = mDh_reco->Projection(4);
+  auto multi_hist_proj_gen = mDh_gen->Projection(4);
+
+  total_nevents = multi_hist_proj_diff->Integral(1,nbinsmll_diff);
   std::cout <<"For nbinspt="<<nbinspt<<" there are " << total_nevents << " events in the inclusive projection \n";
   
   all_histos_count = 0.0;
@@ -123,20 +99,31 @@ int frame(){
   hfrac = -1.0;  
   efrac = -1.0;
   remaining_nevents = 0.0;
-  
+
+  TCanvas *c1 = new TCanvas("c1","c1",800,600);  
+  c1->Divide(2,1);
+
   for (int pos_eta_bin=1; pos_eta_bin<=nbinseta; pos_eta_bin++){
-    mDh->GetAxis(0)->SetRange(pos_eta_bin, pos_eta_bin);
-    for (int pos_pt_bin=1; pos_pt_bin<=nbinspt; pos_pt_bin++){
-      mDh->GetAxis(1)->SetRange(pos_pt_bin, pos_pt_bin);
+    mDh_diff->GetAxis(0)->SetRange(pos_eta_bin, pos_eta_bin);
+    mDh_reco->GetAxis(0)->SetRange(pos_eta_bin, pos_eta_bin);
+    mDh_gen->GetAxis(0)->SetRange(pos_eta_bin, pos_eta_bin);
+    for (int pos_pt_bin=1; pos_pt_bin<=nbinspt; pos_pt_bin++){   
+      mDh_diff->GetAxis(1)->SetRange(pos_pt_bin, pos_pt_bin);
+      mDh_reco->GetAxis(1)->SetRange(pos_pt_bin, pos_pt_bin);
+      mDh_gen->GetAxis(1)->SetRange(pos_pt_bin, pos_pt_bin);
       for (int neg_eta_bin=1; neg_eta_bin<=nbinseta; neg_eta_bin++){
-	mDh->GetAxis(2)->SetRange(neg_eta_bin, neg_eta_bin);
+	mDh_diff->GetAxis(2)->SetRange(neg_eta_bin, neg_eta_bin);
+	mDh_reco->GetAxis(2)->SetRange(neg_eta_bin, neg_eta_bin);
+        mDh_gen->GetAxis(2)->SetRange(neg_eta_bin, neg_eta_bin);
 	for (int neg_pt_bin=1; neg_pt_bin<=nbinspt; neg_pt_bin++){
-	  mDh->GetAxis(3)->SetRange(neg_pt_bin, neg_pt_bin);
+	  mDh_diff->GetAxis(3)->SetRange(neg_pt_bin, neg_pt_bin);
+          mDh_reco->GetAxis(3)->SetRange(neg_pt_bin, neg_pt_bin);
+          mDh_gen->GetAxis(3)->SetRange(neg_pt_bin, neg_pt_bin);
 	  all_histos_count++;
 	  
-	  delete gROOT->FindObject("multi_data_frame_proj_4");
-	  multi_hist_proj = mDh->Projection(4);
-	  nevents = multi_hist_proj->Integral(1,nbinsmll_diff);
+	  delete gROOT->FindObject("multi_data_histo_diff_proj_4");
+	  multi_hist_proj_diff = mDh_diff->Projection(4);
+	  nevents = multi_hist_proj_diff->Integral(1,nbinsmll_diff);
 	  //std::cout<<nevents<<"\n";
 
 	  if (nevents < 100.0){ 
@@ -145,16 +132,48 @@ int frame(){
 	    remaining_nevents += nevents;
 	    
 	    name = stringify_name(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin);
-	    title = stringify_title(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin, etabinranges, ptbinranges);
 	    
-	    fitresult = fitHisto(multi_hist_proj);
+	    fitresult = fitHisto(multi_hist_proj_diff, 2);
 	    GenRecoFit[name] = fitresult;
 	    mean->SetBinError(mean->Fill(name.c_str(), fitresult[0]), fitresult[2]);
 	    sigma->SetBinError(sigma->Fill(name.c_str(), fitresult[1]), fitresult[3]);
 	    
-	    multi_hist_proj->SetName(name.c_str());
-	    multi_hist_proj->SetTitle(title.c_str());
-	    f2->WriteObject(multi_hist_proj, name.c_str());	    
+	    multi_hist_proj_diff->SetName(name.c_str());
+	    multi_hist_proj_diff->SetTitle(("reco - gen mll " + stringify_title(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin, etabinranges, ptbinranges)).c_str());
+	    //delete gROOT->FindObject("c1");
+	    c1->cd(1);
+	    multi_hist_proj_diff->Draw();
+	    //f2->WriteObject(multi_hist_proj_diff, name.c_str());	    
+
+	    c1->cd(2);
+	    delete gROOT->FindObject("multi_data_histo_reco_proj_4");
+	    multi_hist_proj_reco = mDh_reco->Projection(4);
+	    multi_hist_proj_reco->SetLineColor(kBlue);
+	    fitresult = fitHisto(multi_hist_proj_reco, 4);
+            multi_hist_proj_reco->SetTitle(("reco and gen mll "+ stringify_title(pos_eta_bin, pos_pt_bin, neg_eta_bin, neg_pt_bin, etabinranges, ptbinranges)).c_str());
+	    multi_hist_proj_reco->Draw();
+	    delete gROOT->FindObject("multi_data_histo_gen_proj_4");
+	    multi_hist_proj_gen = mDh_gen->Projection(4);
+	    fitresult = fitHisto(multi_hist_proj_gen, 2);
+	    multi_hist_proj_gen->SetLineColor(kRed);
+            multi_hist_proj_gen->Draw("SAME");
+
+	    auto leg = new TLegend(0.10, 0.68, 0.70, 0.90);
+	    leg->SetFillStyle(0);
+	    leg->SetBorderSize(0);
+	    leg->SetTextSize(0.035);
+	    leg->SetFillColor(10);
+	    leg->SetNColumns(1);
+	    leg->SetHeader("");
+
+	    leg->AddEntry(multi_hist_proj_reco, "reco", "l");
+	    leg->AddEntry(multi_hist_proj_gen, "gen", "l");
+	    leg->Draw("");
+
+	    c1->cd();
+
+	    f2->WriteObject(c1, name.c_str());
+
 	  }
 
 	}
@@ -168,11 +187,11 @@ int frame(){
 
   mean->SetStats(0);
   mean->LabelsDeflate();
-  f1->WriteObject(mean, "mean");
+  f1->WriteObject(mean, "mean_diff");
   
   sigma->SetStats(0);
   sigma->LabelsDeflate();
-  f1->WriteObject(sigma, "sigma");
+  f1->WriteObject(sigma, "sigma_diff");
 
   return 0; 
 }
